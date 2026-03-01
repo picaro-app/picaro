470472c15872de4ff673fed75bf3c83c3990dba5
mac <mac@MacBook-Pro.local>
2026-03-02 02:29:11 +0530

    added photoghrapher signup

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-from database import engine, Base
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import engine, Base, SessionLocal
 import models
 
 app = FastAPI()
@@ -8,6 +9,38 @@ app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
 
+# DB dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/")
 def home():
     return {"message": "Picaro backend running 🚀"}
+
+
+@app.post("/signup")
+def signup(name: str, email: str, password: str, db: Session = Depends(get_db)):
+
+    existing_user = db.query(models.Photographer).filter(
+        models.Photographer.email == email
+    ).first()
+
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    new_user = models.Photographer(
+        name=name,
+        email=email,
+        password=password
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"message": "Photographer account created successfully"}
